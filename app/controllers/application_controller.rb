32472c75d8c5a88c3848c50d1c4abf99898a0d09
Ralf Wirdemann <ralf.wirdemann@codekeepers.de>
2021-11-15 21:10:38 +0100

    Highlights active menu item

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
 
-  helper_method :current_user
+  helper_method :current_user, :active_menu
 
   def current_user
     User.find_by(id: session[:user_id])
+  end
+
+  def active_menu(controller)
+    if params[:controller] == controller 
+      return 'has-background-primary'
+    end
+    ""
   end
 end
