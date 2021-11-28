d98ab8535c4a91130d5f20d4117541ee9c755865
Ralf Wirdemann <ralf.wirdemann@codekeepers.de>
2021-11-28 22:24:51 +0100

    Adds infinite scrolling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,23 @@
 class SessionsController < ApplicationController
   def index
-
-  unless current_user.nil?
-    friends = Friendship.where("user_id = ?", current_user.id).map { |f| f.friend_id }
-    sessions = Session.order(:day).where(
-      "visibility = ? OR user_id = ? OR user_id IN (?)", "public", current_user.id, friends)
-  else        
-    sessions = Session.order(:day).where("visibility = ?", "public")
-  end
+    unless current_user.nil?
+      friends = Friendship.where("user_id = ?", current_user.id).map { |f| f.friend_id }
+      sessions = Session.order(:day).where(
+        "visibility = ? OR user_id = ? OR user_id IN (?)", "public", current_user.id, friends)
+    else        
+      sessions = Session.order(:day).where("visibility = ?", "public") 
+    end
 
     @pagy, @sessions = pagy(sessions)
     @spots = Spot.order(:name)
     @session = Session.new(:day => Date.today)
     @user = User.new
+    respond_to do |format|
+      format.html
+      format.json {
+        render json: {entries: render_to_string(partial: "sessions", formats: [:html]), pagination: view_context.pagy_bulma_nav(@pagy)}
+      }
+    end
   end
 
   def create
