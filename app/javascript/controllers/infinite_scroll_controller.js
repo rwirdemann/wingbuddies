abae3303c1f1bc4b110853aa93b424324119101c
Ralf Wirdemann <ralf.wirdemann@aldautomotive.com>
2021-11-29 13:40:17 +0100

    Uses IntersectionObserver for scrolling detection

--- app/javascript/controllers/infinite_scroll_controller.js
+++ app/javascript/controllers/infinite_scroll_controller.js
@@ -1,33 +1,48 @@
-import { Controller } from "stimulus"
+import {Controller} from "stimulus"
 import Rails from "@rails/ujs"
 
 export default class extends Controller {
-  static targets = ["entries", "pagination"]
- 
-  scroll() {    
-    let next_page = this.paginationTarget.querySelector("a[rel='next']")
-    if (next_page == null) {
-      return
+    static targets = ["entries", "pagination"]
+
+    initialize() {
+        let options = {
+            rootMargin: '200px',
+        }
+
+        this.intersectionObserver = new IntersectionObserver(entries => this.processIntersectionEntries(entries), options)
     }
 
-    let url = next_page.href
-    var body = document.body, html = document.documentElement
-
-    var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight)
-    if (window.scrollY >= height - window.innerHeight) {
-      this.loadMore(url)
+    connect() {
+        this.intersectionObserver.observe(this.paginationTarget)
     }
-  }
 
-  loadMore(url) {
-    Rails.ajax({
-      type: 'GET',
-      url: url,
-      dataType: 'json',
-      success: (data) => {
-        this.entriesTarget.insertAdjacentHTML('beforeend', data.entries)
-        this.paginationTarget.innerHTML = data.pagination
-      }
-    })
-  }
+    disconnect() {
+        this.intersectionObserver.unobserve(this.paginationTarget)
+    }
+
+    processIntersectionEntries(entries) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                this.loadMore()
+            }
+        })
+    }
+
+    loadMore() {
+        let next_page = this.paginationTarget.querySelector("a[rel='next']")
+        if (next_page == null) {
+            return
+        }
+        let url = next_page.href
+
+        Rails.ajax({
+            type: 'GET',
+            url: url,
+            dataType: 'json',
+            success: (data) => {
+                this.entriesTarget.insertAdjacentHTML('beforeend', data.entries)
+                this.paginationTarget.innerHTML = data.pagination
+            }
+        })
+    }
 }
