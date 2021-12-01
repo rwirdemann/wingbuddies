6bec87c58fc9ca11eb0430df109ada60c4a32505
Ralf Wirdemann <ralf.wirdemann@codekeepers.de>
2021-12-01 21:10:52 +0100

    Adds some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ralf = User.create(name: 'Ralf')
+
+heiligenhafen = Spot.create(name: 'Heiligenhafen', windfinder: 'https://www.windfinder.com/weatherforecast/heiligenhafen')
+hansthom = Spot.create(name: 'Hanstholm', windfinder: 'https://www.windfinder.com/weatherforecast/hanstholm_fischfabrik')
+
+Session.create(day: Date.new(2021, 12, 1), spot: hansthom, visibility: 'public', user: ralf)
