21e9f86e0f979cf24e9b7d2faf32b436ab1af4fb
Ralf Wirdemann <ralf.wirdemann@codekeepers.de>
2021-12-01 21:12:33 +0100

    Updates restart to passenger

--- restart.sh
+++ restart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-kill $(lsof -t -i:8080)
 git pull
-RAILS_SERVE_STATIC_FILES=true bin/rails server -b=0.0.0.0 -p=8080 -e=production -d
+bundle install --deployment --without development test
+bundle exec rake assets:precompile db:migrate RAILS_ENV=production
+passenger-config restart-app $(pwd)
