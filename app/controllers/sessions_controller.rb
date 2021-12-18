59a7d735ffcc032de2ebff114c7c5dcbba3f3e3e
Ralf Wirdemann <ralf.wirdemann@codekeepers.de>
2021-12-18 09:18:20 +0000

    Reformats code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,38 +1,47 @@
 class SessionsController < ApplicationController
   def index
     unless current_user.nil?
-      friends = Friendship.where("user_id = ?", current_user.id).map { |f| f.friend_id }
-      sessions = Session.order(:day).where(
-        "visibility = ? OR user_id = ? OR user_id IN (?)", "public", current_user.id, friends)
-    else        
-      sessions = Session.order(:day).where("visibility = ?", "public") 
+      friends =
+        Friendship.where('user_id = ?', current_user.id).map { |f| f.friend_id }
+      sessions =
+        Session
+          .order(:day)
+          .where(
+            'visibility = ? OR user_id = ? OR user_id IN (?)',
+            'public',
+            current_user.id,
+            friends,
+          )
+    else
+      sessions = Session.order(:day).where('visibility = ?', 'public')
     end
 
     @pagy, @sessions = pagy(sessions)
     @spots = Spot.order(:name)
-    @session = Session.new(:day => Date.today)
+    @session = Session.new(day: Date.today)
     @user = User.new
     respond_to do |format|
       format.html
-      format.json {
-        render json: {entries: render_to_string(partial: "sessions", formats: [:html]), 
-                      pagination: view_context.pagy_bulma_nav(@pagy)}
-      }
+      format.json do
+        render json: {
+          entries:
+            render_to_string(partial: 'sessions', formats: [:html]),
+          pagination: view_context.pagy_bulma_nav(@pagy),
+        }
+      end
     end
   end
 
   def create
-    @session = Session.new(session_params)    
+    @session = Session.new(session_params)
     if params[:spotname].present?
-      spot = Spot.create(:name => params[:spotname])
+      spot = Spot.create(name: params[:spotname])
       @session.spot = spot
     end
     @session.user = current_user
     @session.visibility = params[:visibility]
 
-    if @session.save
-      redirect_to sessions_path
-    end
+    redirect_to sessions_path if @session.save
   end
 
   def join
@@ -56,6 +65,7 @@ class SessionsController < ApplicationController
   end
 
   private
+
   def session_params
     params.require(:session).permit(:spot_id, :day)
   end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,37 +3,46 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 import "@fortawesome/fontawesome-free/js/all";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
 
-document.addEventListener('DOMContentLoaded', () => {
-    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
-        const $notification = $delete.parentNode;
+document.addEventListener("DOMContentLoaded", () => {
+    (document.querySelectorAll(".notification .delete") || []).forEach(
+        ($delete) => {
+            const $notification = $delete.parentNode;
 
-        $delete.addEventListener('click', () => {
-            $notification.parentNode.removeChild($notification);
-        });
+            $delete.addEventListener("click", () => {
+                $notification.parentNode.removeChild($notification);
+            });
+        }
+    );
+
+    var loginLink = document.querySelector("#loginLink");
+    var loginName = document.querySelector("#user_name");
+    var loginForm = document.querySelector("#loginForm");
+    var registerLink = document.querySelector("#registerLink");
+    var registerForm = document.querySelector("#registerForm");
+    loginLink.addEventListener("click", () => {
+        loginLink.parentNode.classList.add("is-active");
+        registerLink.parentNode.classList.remove("is-active");
+        loginForm.style.display = "block";
+        registerForm.style.display = "none";
+        loginName.focus();
     });
-    
 
-    var loginLink = document.querySelector('#loginLink');
-    var registerLink = document.querySelector('#registerLink');
-    loginLink.addEventListener('click', () => {
-        loginLink.parentNode.classList.toggle("is-active")
-        registerLink.parentNode.classList.toggle("is-active")
-    });
-
-    registerLink.addEventListener('click', () => {
-        registerLink.parentNode.classList.toggle("is-active")
-        loginLink.parentNode.classList.toggle("is-active")
+    registerLink.addEventListener("click", () => {
+        loginLink.parentNode.classList.remove("is-active");
+        registerLink.parentNode.classList.add("is-active");
+        loginForm.style.display = "none";
+        registerForm.style.display = "block";
     });
 });
 
-import 'controllers'
+import "controllers";
