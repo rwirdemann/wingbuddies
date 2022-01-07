7720132d1ab97dbfc8defc0c0babc7d149aa2d14
Ralf Wirdemann <ralf.wirdemann@codekeepers.de>
2022-01-07 09:38:31 +0100

    Adds event handler to close modal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -36,6 +36,14 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+        const $target = $close.closest('.modal');
+
+        $close.addEventListener('click', () => {
+            closeModal($target);
+        });
+    });
+    
     document.addEventListener('keydown', (event) => {
         const e = event || window.event;
 
